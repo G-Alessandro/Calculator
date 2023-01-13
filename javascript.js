4e5c00f4bb5b90f3b99f4eb650e878e7c1ad9cca
G-Alessandro <galloalessandro77@gmail.com>
2023-01-13 19:10:24 +0100

    Removed function add()-subtract()-multiply()-divide()

--- javascript.js
+++ javascript.js
@@ -64,28 +64,6 @@ function operate () {
     numToOperate = [];
 }
 
-// function add (...args) {
-//     let result = 0;
-//     for (let arg of args) result += arg;
-//     return result
-// };
-
-// function subtract (...args) {
-//     let result = 0;
-//     for (let arg of args) result -= arg;
-//     return result
-// };
-
-// function multiply (...args) {
-//     const result = args.reduce((result, n) => result * n )
-//     return result
-// };
-
-// function divide (...args) {
-//     const result = args.reduce((result, n) => result / n )
-//     return result
-// };
-
 btnClear.addEventListener('click', function (){
     btnClicked = [];
     numToUse = [];
